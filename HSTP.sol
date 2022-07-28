c74fa3d23c7c085995c20b2063d2b3c2f18d1126
cagataycali <cagataycali@icloud.com>
2022-07-28 05:31:39 +0200

    Do the iteraton first, REST to GRAPHQL

--- HSTP.sol
+++ HSTP.sol
@@ -1,53 +1,41 @@
-// SPDX-License-Identifier: GNU-3.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-// Author: cagataycali@icloud.com
 
 // Stateless Hyper Service Transfer Protocol for on-chain services.
 abstract contract HTSP {
     struct Route {
         string path;
-        string method;
+        string operation;
+        string payload;
     }
     struct Request {
         string path;
-        string[] params;
-        string[] headers;
+        string payload;
+        string headers;
+        string cookies;
     }
     struct Response {
         string status;
         string body;
         string headers;
     }
-    // Mapping for routes: {'/': 'GET', '/', 'PUT'}
+    // Mapping for routes: {'/': 'query', '/', 'mutation'}
     mapping(string => string) public routes;
 
     // Main reply method for all requests,
-    function reply(string memory method, string memory path)
+    function reply(Route memory route)
         public
         virtual
-        returns (Response memory response) {
-            // if (method === "GET") { return this.get(Request memory request); }
-            // if (method === "PUT") { return this.get(Request memory request); }
-            // if (method === "REMOVE") { return this.get(Request memory request); }
-            // return this._500();
-        }
+        returns (string memory);
 
-    // Methods
-    function get(Request memory request)
+    // Operation
+    function query(Request memory request)
         public
         virtual
         returns (Response memory response);
 
-    function put(Request memory request)
-        public
-        virtual
-        returns (Response memory response);
-
-    function remove(Request memory request)
+    function mutation(Request memory request)
         public
         virtual
         returns (Response memory response);
 }
-
-// Each route will have access NFT's. 
-// If the access NFT exists on the caller, caler could call the service's method.
